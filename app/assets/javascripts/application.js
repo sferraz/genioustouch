99c98ea88733e64cd33af92ddbd8ea61f3b8e314
Sandro Ferraz <sandroferraz@MacBook-Pro-de-Sandro.local>
2015-11-20 00:11:38 +0000

    trying deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+// require_tree .
 //= require highcharts/highstock
 //= require highcharts/highcharts                                                           
 //= require highcharts/highcharts-more 
